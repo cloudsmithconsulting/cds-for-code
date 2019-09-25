15bd78228871efeee4153f097826df465e74b506
Ira Mellor <ira.mellor@cloudsmith.consulting>
2019-09-25 15:08:43 -0400

    change log message

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('[CloudSmithVSCode]: extension:activate');
+	console.log('[CloudSmith]: extension:activate');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
