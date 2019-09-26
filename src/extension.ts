a27eb8de8bc03d9b9a37e5d974ba0a5d47c9fd60
Ira Mellor <ira.mellor@cloudsmith.consulting>
2019-09-25 23:29:57 -0400

    now it shells out to terminal. needs tested against crm instance

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as shelljs from 'shelljs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -53,29 +52,23 @@ export function activate(context: vscode.ExtensionContext) {
 				const codeFilePath = `${rootPath}\\XrmEntities.cs`;
 
 				// setup the command text
-				const commandToExecute = `${svcUtilPath} `
+				const commandToExecute = `& "${svcUtilPath}" `
 					+ `/url:http://crmserver/test/XRMServices/2011/Organization.svc `
 					+ `/username:missioncommand `
-					+ `/password:$mokingTir33 `
+					+ `/password:"$mokingTir33" `.replace('$', '`$') // $ is a problem in powershell
 					+ `/domain:CONTOSO `
 					+ `/namespace:CloudSmith.Dynamics365.SampleTests `
 					+ `/out:${codeFilePath}`;
-
-				// execute the command
-				const cmd = shelljs.exec(commandToExecute);
-
-				if (cmd.code === 0) { // check status (zero is no errors)
-					vscode.window.showInformationMessage(
-						`Success! Your entites were generated to ${codeFilePath}`
-					);
-				} else {
-					// write error to console
-					console.error(cmd.stdout);
-					// show error message
-					vscode.window.showErrorMessage(
-						`Error generating your entites to ${codeFilePath}`
-					);
-				}
+				
+				// build a powershell terminal
+				const terminal = vscode.window.createTerminal({
+					name: "CloudSmith: Dynamics Terminal",
+					// make sure we get powershell
+					shellPath: "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+				});
+				// show it and execute the command
+				terminal.show();
+				terminal.sendText(commandToExecute);
 			}
 		}) // <-- no semi-colon, comma starts next command registration
 
