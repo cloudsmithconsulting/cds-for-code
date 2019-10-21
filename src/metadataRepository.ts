bb1f305675e8506de1da5ee3cf9a6523ef60d962
Brandon Kelly <brandon.kelly@cloudsmith.consulting>
2019-10-21 13:23:55 -0400

    refactoring complete

--- src/metadataRepository.ts
+++ src/metadataRepository.ts
@@ -15,43 +15,39 @@ export default class MetadataRepository
 
     private webapi: DynamicsWebApiClient;
 
-    public async retrieveEntities(solutionId?:string) : Promise<any[]>
+    public retrieveEntities(solutionId?:string) : Promise<any[]>
     {
-        let solutionQuery:DynamicsWebApi.RetrieveRequest;
-        let solution:any;
         let entitiesQuery:DynamicsWebApi.RetrieveMultipleRequest = {
             filter: "IsIntersect eq false"
         };
 
-        if (solutionId)
-        {
-            solutionQuery = {
-                collection: "solutions",
-                id: solutionId,
-                expand: [ { property: "solution_solutioncomponent", filter: "componenttype eq 1" } ]
-            };    
-
-            solution = await this.webapi.retrieveRequest(solutionQuery);
-
-            if (!solution || !solution.solution_solutioncomponent || solution.solution_solutioncomponent.length === 0)
-            {
-                return null;
+        return this.webapi.retrieveEntitiesRequest(entitiesQuery)
+            .then(entitiesResponse => {
+                if (solutionId) {
+                    let solutionQuery:DynamicsWebApi.RetrieveRequest = {
+                        collection: "solutions",
+                        id: solutionId,
+                        expand: [ { property: "solution_solutioncomponent", filter: "componenttype eq 1" } ]
+                    };    
+                    
+                    return this.webapi.retrieveRequest(solutionQuery).then(solution => {
+                        if (!solution || !solution.solution_solutioncomponent || solution.solution_solutioncomponent.length === 0)
+                        {
+                            return null;
+                        }
+            
+                        let components = new TS.TS.Linq.Enumerator(solution.solution_solutioncomponent);
+                        let filteredList = components
+                            .join(new TS.TS.Linq.Enumerator(entitiesResponse.value), c => c["objectid"], e => e["MetadataId"], (c, e) => e)
+                            .orderBy(e => e["LogicalName"])
+                            .toArray();
+            
+                        return filteredList;
+                    });           
+                } else {
+                    return new TS.TS.Linq.Enumerator(entitiesResponse.value).orderBy(e => e["LogicalName"]).toArray();
+                }
             }
-
-            let entities = await this.webapi.retrieveEntitiesRequest(entitiesQuery);
-            let components = new TS.TS.Linq.Enumerator(solution.solution_solutioncomponent);
-            let filteredList = components.join(new TS.TS.Linq.Enumerator(entities.value), c => c["objectid"], e => e["MetadataId"], (c, e) => e).toArray();
-
-            return filteredList;
-        }
-        else
-        {
-            //TODO: Fix this so that it cross references with the solutioncomponents query above.
-            return this.webapi.retrieveEntitiesRequest(entitiesQuery)
-                .then(response => 
-                    //new TS.TS.Linq.Enumerator(response.value).where(e => e["IsIntersect"] === false).orderBy(e => e["LogicalName"]).toArray()
-                    new TS.TS.Linq.Enumerator(response.value).orderBy(e => e["LogicalName"]).toArray()
-                );   
-        }
+        );   
     }
 }
