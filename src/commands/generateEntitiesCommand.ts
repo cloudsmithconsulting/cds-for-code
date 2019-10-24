af4ac09051c87bf2d587ed83ac1d932196174bea
Ira Mellor <ira.mellor@cloudsmith.consulting>
2019-10-24 10:11:18 -0400

    refactor commands to be more turse without so many brackets

--- src/commands/generateEntitiesCommand.ts
+++ src/commands/generateEntitiesCommand.ts
@@ -1,13 +1,13 @@
-import * as vscode from 'vscode';
 import * as path from 'path';
+import { TS } from 'typescript-linq/TS';
+import * as vscode from 'vscode';
 import * as cs from '../cs';
 import ExtensionConfiguration from '../helpers/ExtensionConfiguration';
-import { IWireUpCommands } from '../wireUpCommand';
-import { Utilities } from '../helpers/Utilities';
-import { Terminal } from '../helpers/Terminal';
-import DiscoveryRepository from '../repositories/discoveryRepository';
 import QuickPickOption from '../helpers/QuickPicker';
-import { TS } from 'typescript-linq/TS';
+import { Terminal } from '../helpers/Terminal';
+import { Utilities } from '../helpers/Utilities';
+import DiscoveryRepository from '../repositories/discoveryRepository';
+import { IWireUpCommands } from '../wireUpCommand';
 
 export default class GenerateEntitiesCommand implements IWireUpCommands {
     public wireUpCommands(context: vscode.ExtensionContext, config: vscode.WorkspaceConfiguration) {
@@ -20,38 +20,27 @@ export default class GenerateEntitiesCommand implements IWireUpCommands {
         // now wire a command into the context
         context.subscriptions.push(
             vscode.commands.registerCommand(cs.dynamics.powerShell.generateEntities, async (config?:DynamicsWebApi.Config, path?:string, outputFileName?: string, namespace?: string) => { // Match name of command to package.json command
-                if (!config) {
-					config = await DiscoveryRepository.getOrgConnections(context)
-						.then(orgs => new TS.Linq.Enumerator(orgs).select(org => new QuickPickOption(org.name, org.webApiUrl, undefined, org)).toArray())
-						.then(options => vscode.window.showQuickPick(options, { placeHolder: "Choose a Dynamics 365 Organization", canPickMany: false, ignoreFocusOut: true}))
-                        .then(chosen => chosen.context);
-                        
-                    if (!config) { return; }
-                }
-                
-                if (!path) {
-                    path = await vscode.window
+                config = config || await DiscoveryRepository.getOrgConnections(context)
+                    .then(orgs => new TS.Linq.Enumerator(orgs).select(org => new QuickPickOption(org.name, org.webApiUrl, undefined, org)).toArray())
+                    .then(options => vscode.window.showQuickPick(options, { placeHolder: "Choose a Dynamics 365 Organization", canPickMany: false, ignoreFocusOut: true}))
+                    .then(chosen => chosen.context);
+                if (!config) { return; }
+
+                path = path || await vscode.window
                     .showOpenDialog({canSelectFolders: true, canSelectFiles: false, canSelectMany: false, defaultUri: workspaceFolder.uri})
                     .then(async pathUris => pathUris[0].fsPath);
+                if (Utilities.IsNullOrEmpty(path)) { return; }
 
-                    if (Utilities.IsNullOrEmpty(path)) { return; }
-                }
+                outputFileName = outputFileName || await vscode.window
+                    .showInputBox({ prompt: 'Please enter the output file name', value: 'XrmEntities.cs' })
+                    .then(chosen => chosen);
+                if (Utilities.IsNullOrEmpty(outputFileName)) { return; }
+                
 
-                if (!outputFileName) {
-                    outputFileName = await vscode.window
-                        .showInputBox({ prompt: 'Please enter the output file name', value: 'XrmEntities.cs' })
-                        .then(chosen => chosen);
-
-                    if (Utilities.IsNullOrEmpty(outputFileName)) { return; }
-                }
-
-                if (!namespace) {
-                    namespace = await vscode.window
-                        .showInputBox({ prompt: 'Please enter the namespace for the generated code', value: 'XrmEntities' })
-                        .then(chosen => chosen);
-
-                    if (Utilities.IsNullOrEmpty(namespace)) { return; }
-                }
+                namespace = namespace || await vscode.window
+                    .showInputBox({ prompt: 'Please enter the namespace for the generated code', value: 'XrmEntities' })
+                    .then(chosen => chosen);
+                if (Utilities.IsNullOrEmpty(namespace)) { return; }
 
                 // setup the command text
                 const commandToExecute = `.\\Generate-XrmEntities.ps1 `
