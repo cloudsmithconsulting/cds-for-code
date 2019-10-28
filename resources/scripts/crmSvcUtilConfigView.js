daef2d2f3d9ba14534c5ecae20c58fa949669da9
Ira Mellor <ira.mellor@cloudsmith.consulting>
2019-10-28 14:41:42 -0400

    First real cut at UI for CrmSvcUtilConfig dialog

--- resources/scripts/crmSvcUtilConfigView.js
+++ resources/scripts/crmSvcUtilConfigView.js
@@ -2,6 +2,26 @@
 // It cannot access the main VS Code APIs directly.
 (function () {
     const vscode = acquireVsCodeApi();
+    const formElements = document.getElementsByClassName("field-container");
+    const formTypeLabels = document.getElementsByClassName("form-type-label");
+
+    const changeView = window.changeView = function(event, viewName) {
+        if (!event) { return; }
+
+        for (let i = 0; i < formElements.length; i++) {
+            const el = formElements[i];
+            if (el.getAttribute("data-form").indexOf(viewName) === -1) {
+                el.setAttribute("hidden", "hidden");
+            } else {
+                el.removeAttribute("hidden");
+            }
+        }
+
+        for (let i = 0; i < formTypeLabels.length; i++) {
+            const el = formTypeLabels[i];
+            el.innerHTML = viewName;
+        }
+    };
 
     // Handle messages sent from the extension to the webview
     window.addEventListener("message", event => {
