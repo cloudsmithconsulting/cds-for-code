121af155d9ad64fa2582ba664ea4f4a27dbe51a7
Brandon Kelly <brandon.kelly@cloudsmith.consulting>
2019-10-29 09:23:28 -0400

    prestine terminals :D

--- src/views/DynamicsTerminal.ts
+++ src/views/DynamicsTerminal.ts
@@ -5,15 +5,17 @@ import * as fs from 'fs';
 import * as child_process from 'child_process';
 import { SpawnOptions } from 'child_process';
 import Utilities from '../helpers/Utilities';
-import { resolveSoa } from 'dns';
+import { throws } from 'assert';
 
 export class TerminalCommand {
 	private _command:string;
 	private _output:string;
+	private _error:string;
 
-	constructor(command: string, output: string) {
+	constructor(command: string, output?: string, error?:string) {
 		this._command = command;
 		this._output = output;
+		this._error = error;
 	}
 
 	get command():string { 
@@ -22,6 +24,10 @@ export class TerminalCommand {
 
 	get output():string {
 		return this._output;
+	}
+
+	get error():string {
+		return this._error;
 	}
 }
 export class Terminal implements vscode.Terminal {
@@ -37,18 +43,29 @@ export class Terminal implements vscode.Terminal {
 	private _process;
 	private _isMasked: boolean = false;
 	private _inputCommand:string = "";
-	private _output:string[] = [];
-	private _error:string[] = [];
-	private _position:number = 0;
+	private _outputBuffer:string[] = [];
+	private _errorBuffer:string[] = [];
+	private _cursorPosition:number = 0;
+	private _commandBuffer:TerminalCommand[] = [];
 
-	onDidWrite: vscode.Event<string> = this._onDidWrite.event;
-	onDidOpen: vscode.Event<void> = this._onDidOpen.event;
-	onDidClose: vscode.Event<void> = this._onDidClose.event;
-	onDidReceiveInput: vscode.Event<string> = this._onDidReceiveInput.event;
-	onDidRunCommand: vscode.Event<TerminalCommand> = this._onDidRunCommand.event;
-	onDidError: vscode.Event<TerminalCommand> = this._onDidError.event;
+	public onDidWrite: vscode.Event<string> = this._onDidWrite.event;
+	public onDidOpen: vscode.Event<void> = this._onDidOpen.event;
+	public onDidClose: vscode.Event<void> = this._onDidClose.event;
+	public onDidReceiveInput: vscode.Event<string> = this._onDidReceiveInput.event;
+	public onDidRunCommand: vscode.Event<TerminalCommand> = this._onDidRunCommand.event;
+	public onDidError: vscode.Event<TerminalCommand> = this._onDidError.event;
 	
+	private static _terminals:Terminal[] = [];
 	static readonly defaultTerminalName: string = "CloudSmith Terminal";
+	static readonly maximumCommandBufferSize: number = 20;
+
+	static get ActiveTerminals(): Terminal[] {
+		return this._terminals;
+	}
+
+	get commandBuffer(): TerminalCommand[] {
+		return this._commandBuffer;
+	}
 
 	get isMasked(): boolean {
 		return this._isMasked;
@@ -62,8 +79,18 @@ export class Terminal implements vscode.Terminal {
 		return this._path;
 	}
 
+	set path(value:string) { 
+		if (value && value !== this._path) {
+			this.sendText(`cd '${value}'`, true);
+		}
+	}
+
 	get processId(): Thenable<number> {
 		return this._process.pid;
+	}
+
+	get cursorPosition(): number {
+		return this._cursorPosition;
 	}
 
 	constructor(options?:vscode.TerminalOptions, singleton:boolean = true) 
@@ -73,20 +100,30 @@ export class Terminal implements vscode.Terminal {
 		}
 	}
 
-	create(options?:vscode.TerminalOptions, singleton:boolean = true): Promise<Terminal>
+	create<T extends vscode.Terminal>(options?:vscode.TerminalOptions, singleton:boolean = true): Promise<T>
 	{
 		options = options || this._options;
-		const index = vscode.window.terminals.findIndex(t => t.name === options.name);
 
-		if (index !== -1 && singleton) {
-			if (options.name === this.name) {
-				return Promise.resolve(this);
-			} else {
-				return Promise.resolve(<Terminal>vscode.window.terminals[index]);
-			}
-		}
+		let index = Terminal.ActiveTerminals.findIndex(t => t.name === options.name);
+		let currentTerminal:T;
 
 		if (index === -1) {
+			index = vscode.window.terminals.findIndex(t => t.name === options.name);
+
+			if (index !== -1) {
+				currentTerminal = <T>vscode.window.terminals.find(t => t.name === options.name);
+			}
+		} else { 
+			currentTerminal = <T><unknown>Terminal.ActiveTerminals[index];
+		}
+		
+		if (currentTerminal && singleton) {
+			currentTerminal.show(true);
+
+			if (options.cwd) {
+				this.sendText(`cd '${options.cwd}'`, true);
+			}
+		} else {
 			let spawnOptions:SpawnOptions = {
 				cwd: options.cwd.toString(),				
 			};
@@ -97,67 +134,58 @@ export class Terminal implements vscode.Terminal {
 					pty:  {
 						onDidWrite: this._onDidWrite.event,
 						open: () => {
+							Terminal.ActiveTerminals.push(this);
+							this.show(true);
+
 							this._process = child_process.spawn(options.shellPath, spawnOptions);
 							this._process.stdout.on("data", data => {
-								this._output.push(data.toString().replace(this._inputCommand, "").replace("\r", "").replace("\n", ""));
-				
-								const result = /(PS )(.*)(> )/i.exec(data.toString());
-
-								if (result && result.length > 2) {
-									this._path = result[2];
-
-									if (this._output[this._output.length - 1] === result[0]) {
-										this._output.pop();
-									} else {
-										this._output[this._output.length - 1] = this._output[this._output.length - 1].replace(result[0], "");
-									}
-
-									if (resolve) { 
-										resolve(this); 
-									}
-
-									if (!Utilities.IsNullOrEmpty(this._inputCommand) && this._output.length > 0) {
-										 this._onDidRunCommand.fire(new TerminalCommand(this._inputCommand, this._output.join("")));
-
-										this._inputCommand = "";
-										this._output = [];
-									}
-								}
+								this._outputBuffer.push(data.toString().replace(this._inputCommand, "").replace("\r", "").replace("\n", ""));
 
 								if (!Utilities.IsNullOrEmpty(this._inputCommand)) {
 									this.write(data.toString().replace(this._inputCommand, ""));
 								} else {
 									this.write(data.toString());
 								}
+
+								const prompt = /(PS )(.*)(> )/i.exec(data.toString());
+
+								if (prompt && prompt.length > 2) {
+									if (this._path !== prompt[2]) {
+										this._path = prompt[2];
+									}
+
+									if (this._outputBuffer[this._outputBuffer.length - 1] === prompt[0]) {
+										this._outputBuffer.pop();
+									} else {
+										this._outputBuffer[this._outputBuffer.length - 1] = this._outputBuffer[this._outputBuffer.length - 1].replace(prompt[0], "");
+									}
+
+									this.resolveIncomingCommand<T>(resolve, reject);
+								}
 							});
 							this._process.stderr.on("data", data => {
-								this._error.push(data.toString().replace(this._inputCommand, "").replace("\r", "").replace("\n", ""));
-				
-								const result = /(PS )(.*)(> )/i.exec(data.toString());
-
-								if (result && result.length > 2) {
-									if (reject) { reject(this); }
-
-									this._path = result[2];
-
-									if (this._error[this._error.length - 1] === result[0]) {
-										this._error.pop();
-									} else {
-										this._error[this._error.length - 1] = this._error[this._error.length - 1].replace(result[0], "");
-									}
-
-									if (!Utilities.IsNullOrEmpty(this._inputCommand) && this._error.length > 0) {
-										this._onDidError.fire(new TerminalCommand(this._inputCommand, this._error.join("")));
-
-										this._inputCommand = "";
-										this._error = [];
-									}
-								}
+								this._errorBuffer.push(data.toString().replace(this._inputCommand, "").replace("\r", "").replace("\n", ""));
 
 								if (!Utilities.IsNullOrEmpty(this._inputCommand)) {
 									this.writeColor(3, data.toString().replace(this._inputCommand, ""));
 								} else {
 									this.writeColor(3, data.toString());
+								}
+
+								const prompt = /(PS )(.*)(> )/i.exec(data.toString());
+
+								if (prompt && prompt.length > 2) {
+									if (this._path !== prompt[2]) {
+										this._path = prompt[2];
+									}
+
+									if (this._errorBuffer[this._errorBuffer.length - 1] === prompt[0]) {
+										this._errorBuffer.pop();
+									} else {
+										this._errorBuffer[this._errorBuffer.length - 1] = this._errorBuffer[this._errorBuffer.length - 1].replace(prompt[0], "");
+									}
+
+									this.resolveIncomingCommand<T>(resolve, reject);
 								}
 							});
 				
@@ -165,6 +193,12 @@ export class Terminal implements vscode.Terminal {
 						},
 						close: () => {
 							this._onDidClose.fire();
+
+							const index = Terminal.ActiveTerminals.findIndex(t => t.name === this.name);
+
+							if (index !== -1) {
+								Terminal.ActiveTerminals.splice(index, 1);
+							}
 
 							this._process.stdin.end();
 							this._process.dispose();
@@ -174,7 +208,7 @@ export class Terminal implements vscode.Terminal {
 
 							if (data === '\r') { // Enter
 								this.write('\r\n');
-								this._position = 0;
+								this._cursorPosition = 0;
 
 								if (this._process) {
 									this._process.stdin.write(this._inputCommand + "\r\n"); 
@@ -184,11 +218,11 @@ export class Terminal implements vscode.Terminal {
 							}
 
 							if (data === '\x7f') { // Backspace
-								if (this._inputCommand.length === 0 || this._position === 0) {
+								if (this._inputCommand.length === 0 || this._cursorPosition === 0) {
 									return;
 								}
 
-								this._position--;
+								this._cursorPosition--;
 								this._inputCommand = this._inputCommand.substr(0, this._inputCommand.length - 1);
 								// Move cursor backward
 								this.write('\x1b[D');
@@ -200,11 +234,34 @@ export class Terminal implements vscode.Terminal {
 
 							this.write(data);
 							this._inputCommand += data;
-							this._position++;
+							this._cursorPosition++;
 						}
 					}});
 				});
-			
+		}
+	}
+
+	private resolveIncomingCommand<T extends vscode.Terminal>(resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) {
+		if (!Utilities.IsNullOrEmpty(this._inputCommand) || this._outputBuffer.length > 0 || this._errorBuffer.length > 0) {
+			const command = new TerminalCommand(this._inputCommand, this._outputBuffer.join(""), this._errorBuffer.join(""));
+
+			this._onDidRunCommand.fire(command);
+			this._commandBuffer.push(command);
+
+			if (this._commandBuffer.length > Terminal.maximumCommandBufferSize) {
+				this._commandBuffer.splice(0, this._commandBuffer.length - Terminal.maximumCommandBufferSize);
+			}
+
+			if (resolve && this._errorBuffer.length === 0) {
+				resolve(<T><unknown>this);
+			}
+			else if (reject && this._errorBuffer.length > 0) {
+				reject(<T><unknown>this);
+			}
+
+			this._inputCommand = "";
+			this._outputBuffer = [];
+			this._errorBuffer = [];
 		}
 	}
 
@@ -319,32 +376,23 @@ export class Terminal implements vscode.Terminal {
 export default class DynamicsTerminal implements IWireUpCommands
 {
 	wireUpCommands(context: vscode.ExtensionContext, config?: vscode.WorkspaceConfiguration): void {
-		let terminal:Terminal;
+		const terminal:Terminal = new Terminal();
 
 		context.subscriptions.push(vscode.commands.registerCommand(cs.dynamics.extension.createTerminal, (folder:string):Promise<Terminal> => {
 			if (!folder || !fs.existsSync(folder)) {
 				folder = context.globalStoragePath;
 			}
 
-			const isNew:boolean = !terminal;
-
-			terminal = terminal || new Terminal();
-
-			return terminal.create({
+			return terminal.create<Terminal>({
 					name: Terminal.defaultTerminalName,
 					shellPath: "powershell.exe",
 					cwd: folder 
 				}, true)
 				.then(term => {
-					term.show();
 					term.onDidClose(() => {
 						term.dispose();
 						term = null;
 					});
-
-					if (!isNew) { 
-						term.sendText(`cd ${folder}`, true); 
-					}
 
 					return terminal;
 				});
