cd8536a72e39d7c771c712aec3d60ceff05dbd16
Brandon Kelly <brandon.kelly@cloudsmith.consulting>
2019-10-28 23:16:22 -0400

    command refactoring

--- src/commands/generateEntities.ts
+++ src/commands/generateEntities.ts
@@ -36,8 +36,7 @@ export default class GenerateEntitiesCommand implements IWireUpCommands {
 
                 // build a powershell terminal
                 DynamicsTerminal.showTerminal(path.join(context.globalStoragePath, "\\Scripts\\"))
-                    .then(terminal => {
-                        terminal.text(`.\\Generate-XrmEntities.ps1 `)
+                    .then(terminal => { terminal.text(`.\\Generate-XrmEntities.ps1 `)
                             .text(`-ToolsPath ${coreToolsRoot} `)
                             .text(`-Url "${Utilities.EnforceTrailingSlash(config.webApiUrl)}XRMServices/2011/Organization.svc" `)
                             .text(`-Username "${config.username}" -Password "`)
