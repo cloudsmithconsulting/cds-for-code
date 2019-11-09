626926c37f0c2e031f7f5005ebfac763b41cc30d
Ira Mellor <ira.mellor@cloudsmith.consulting>
2019-11-08 21:30:49 -0500

    autocomplete working

--- resources/scripts/pluginStepView.js
+++ resources/scripts/pluginStepView.js
@@ -4,6 +4,7 @@
 // It cannot access the main VS Code APIs directly.
 (function () {
     const vscode = CloudSmith.acquireVsCodeApi();
+    window.sdkMessages = [];
 
     // Handle messages sent from the extension to the webview
     window.addEventListener("message", event => {
@@ -16,6 +17,10 @@
         }
     });
 
+    function setMessageAutoComplete(sdkMessages) {
+        window.sdkMessages = _.map(sdkMessages, m => m.name);
+    };
+
     function setInitialState(viewModel) {
         setMessageAutoComplete(viewModel.sdkMessages);
         
@@ -25,18 +30,76 @@
     }
 
     $(function() {
-        function validateForm(config) {
+        const $autocompleteItems = $(".autocomplete-items");
+        let currentFocus = -1;
+        $("#Message").keyup(function(e) {
+            const lookup = this.value;
+            // clear all previous items
+            $("div", $autocompleteItems).remove();
+            // get out if there's no work to do
+            if (CloudSmith.Utilities.isNullOrEmpty(lookup)) {
+                currentFocus = -1;
+                return; 
+            }
+            // find all the matching items and append them to the
+            // autocomplete
+            _.each(window.sdkMessages, message => {
+                // see if we have a match
+                if (message.substr(0, lookup.length).toLowerCase() === lookup.toLowerCase()) {
+                    const $button = $("<button class='button button--primary'>");
+                    $button.val(message);
+                    $button.append("<span class='iconify' data-icon='fa:envelope'></span>");
+                    $button.append(`&nbsp;<strong>${message.substr(0, lookup.length)}</strong>${message.substr(lookup.length)}`);
+
+                    $button.click(function() {
+                        $("#Message").val(this.value);
+                        // clear all previous items
+                        $("div", $autocompleteItems).remove();
+                    });
+
+                    const $div = $("<div></div>").append($button)
+                    $autocompleteItems.append($div);
+                }
+            });
+
+            // now look for navigation keys
+            if (e.keyCode == 40) {
+                /*If the arrow DOWN key is pressed,
+                increase the currentFocus variable:*/
+                currentFocus++;
+                /*and and make the current item more visible:*/
+                $("button", $autocompleteItems).removeClass("button--active");
+                $("button", $autocompleteItems).eq(currentFocus).addClass("button--active");
+              } else if (e.keyCode == 38) { //up
+                /*If the arrow UP key is pressed,
+                decrease the currentFocus variable:*/
+                currentFocus--;
+                /*and and make the current item more visible:*/
+                $("button", $autocompleteItems).removeClass("button--active");
+                $("button", $autocompleteItems).eq(currentFocus).addClass("button--active");
+              } else if (e.keyCode == 13) {
+                /*If the ENTER key is pressed, prevent the form from being submitted,*/
+                e.preventDefault();
+                if (currentFocus > -1) {
+                  /*and simulate a click on the "active" item:*/
+                  $("button", $autocompleteItems).eq(currentFocus).click();
+                }
+              }
+        });
+
+        function validateForm(settings) {
             const messages = [];
 
             // put validations in here
-            if (CloudSmith.Utilities.isNullOrEmpty(config.message)) { messages.push('The Message is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.primaryEntity)) { messages.push('The Primary Entity is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.secondEntity)) { messages.push('The Second Entity is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.filteringAttributes)) { messages.push('The Filtering Attributes is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.eventHandler)) { messages.push('The Event Handler is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.stepName)) { messages.push('The Step Name is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.userContext)) { messages.push('The User Context is required'); }
-            if (CloudSmith.Utilities.isNullOrEmpty(config.executionOrder)) { messages.push('The Execution Order is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.message)) { messages.push('The Message is required'); }
+            if (window.sdkMessages.indexOf(cofig.message) === -1) { messages.push('The Message has to exist in message selection'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.primaryEntity)) { messages.push('The Primary Entity is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.secondEntity)) { messages.push('The Second Entity is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.filteringAttributes)) { messages.push('The Filtering Attributes is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.eventHandler)) { messages.push('The Event Handler is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.stepName)) { messages.push('The Step Name is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.userContext)) { messages.push('The User Context is required'); }
+            if (CloudSmith.Utilities.isNullOrEmpty(settings.executionOrder)) { messages.push('The Execution Order is required'); }
 
             // show errors
             CloudSmith.ErrorPanel.showError(messages);
@@ -47,7 +110,7 @@
 
         // wire up submit click handler
         $("#submitButton").click(function() {
-            const config = {
+            const settings = {
                 // work on gathering the form information
                 message: $("#Message").val(),
                 primaryEntity: $("#PrimaryEntity").val(),
@@ -64,7 +127,7 @@
                 offline: $("#Offline").val() && $("#Offline").val().length > 0
             };
 
-            if (!validateForm(config)) return;
+            if (!validateForm(settings)) return;
 
             vscode.postMessage({
                 command: 'saveSdkMessageProcessingStep',
