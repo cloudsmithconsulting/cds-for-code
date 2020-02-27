0932c7ae11d4a0e3ace75a4cc5048f21416a4659
Brandon Kelly <brandon.kelly@cloudsmith.consulting>
2020-02-27 14:47:28 -0500

    dev changes to faker

--- src/commands/cs.cds.data.getFaker.ts
+++ src/commands/cs.cds.data.getFaker.ts
@@ -12,22 +12,105 @@ import DataApiRepository from '../repositories/DataApiRepository';
 
 export type GeneratorDefinition<T> = { name: string, generate: () => T };
 
-let customers = [];
+const cache = {
+	customers: [],
+	lookups: new Map<string, any[]>(),
+	users: [],
+	parties: []
+};
 
 const generators = {
-	0: async (attribute: any): Promise<GeneratorDefinition<boolean>> => { 
+	"Boolean": async (attribute: any): Promise<GeneratorDefinition<boolean>> => { 
 		return { name: attribute.LogicalName, generate: () => faker.random.boolean() }; 
 	},
-	1: async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<string>> => {
-		if (customers?.length === 0) {
+	"Customer": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<string>> => {
+		if (cache.customers?.length === 0) {
 			const api = new DataApiRepository(config);
-			customers = await api.getSampleCustomers();
+			cache.customers = await api.getSampleCustomers();
 		}
 
-		const customer = customers[Math.round(Math.random() * customers.length)];
+		const customer = cache.customers[Math.round(Math.random() * cache.customers.length)];
 
-		 return { name: attribute.LogicalName, generate: () => customer }; 
+		 return { name: `${attribute.LogicalName}_${customer.collection.substring(0, customer.collection.length - 1)}@odata.bind`, generate: () => `/${customer.collection}/${Utilities.Guid.trimGuid(customer.id)}` }; 
 	},
+	"DateTime": async (attribute: any): Promise<GeneratorDefinition<Date>> => {
+		return { name: attribute.LogicalName, generate: () => faker.date.between('1/1/1900', new Date()) };
+	}, 
+	"Decimal": async (attribute: any): Promise<GeneratorDefinition<number>> => {
+		return { name: attribute.LogicalName, generate: () => faker.random.float(faker.random.number(9)) };
+	},
+	"Double": async (attribute: any): Promise<GeneratorDefinition<number>> => {
+		return { name: attribute.LogicalName, generate: () => faker.random.float(faker.random.number(5)) };
+	},
+	"Integer": async (attribute: any): Promise<GeneratorDefinition<number>> => {
+		return { name: attribute.LogicalName, generate: () => faker.random.number({ min: attribute.MinValue, max: attribute.MaxValue }) };
+	},
+	"Lookup": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<any>> => {
+		if (!cache.lookups.has(attribute.Targets)) {
+			const api = new DataApiRepository(config);
+			const value = await api.getLookupValues(attribute.Targets);
+
+			cache.lookups.set(attribute.Targets, value);
+		}
+
+		const possibleValues = cache.lookups.get(attribute.Targets).length;
+
+		return { name: `${attribute.LogicalName}@odata.bind`, generate: () => {
+			const randomValue = cache.lookups.get(attribute.Targets)[faker.random.number(possibleValues) - 1];
+
+			return `/${randomValue.collection}/${Utilities.Guid.trimGuid(randomValue.id)}`;
+		} };
+	},
+	"Memo": async (attribute: any): Promise<GeneratorDefinition<string>> => {
+		return { name: attribute.LogicalName, generate: () => faker.random.words(faker.random.number(100)) };
+	},
+	"Money": async (attribute: any): Promise<GeneratorDefinition<number>> => {
+		return { name: attribute.LogicalName, generate: () => faker.random.float(2) };
+	},
+	"Owner": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<string>> => {
+		if (cache.users?.length === 0) {
+			const api = new DataApiRepository(config);
+			cache.users = await api.getSystemUsers();
+		}
+
+		const user = cache.users[Math.round(Math.random() * cache.users.length)];
+
+		return { name: `${attribute.LogicalName}_${user.collection.substring(0, user.collection.length - 1)}@odata.bind`, generate: () => `/${user.collection}/${Utilities.Guid.trimGuid(user.id)}` }; 
+	},
+	"PartyList": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<any>> => {
+		if (cache.parties?.length === 0) {
+			const api = new DataApiRepository(config);
+			cache.parties = await api.getPartyListMembers();
+		}
+
+		return { name: attribute.LogicalName, generate: () => cache.parties[Math.round(Math.random() * cache.parties.length)] };
+	},
+	"Picklist": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<number>> => {
+		const optionSet = attribute.OptionSet || attribute.GlobalOptionSet;
+		const option = optionSet.Options[Math.round(Math.random() * optionSet.Options.length)];
+
+		return { name: attribute.LogicalName, generate: () => option.Value };
+	}, 
+	"State": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<number>> => {
+		return { name: attribute.LogicalName, generate: () => 0 };
+	}, 	
+	"Status": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<number>> => {
+		return { name: attribute.LogicalName, generate: () => 1 };
+	}, 	
+	"String": async (attribute: any, config: CdsWebApi.Config): Promise<GeneratorDefinition<string>> => {
+		const type = attribute.Format;
+		let value: string;
+
+		switch (type) {
+			case "Email":
+				value = faker.internet.email();
+				break;
+			case "Text":
+				value = faker.lorem.text();
+				break;
+		}
+		return { name: attribute.LogicalName, generate: () => value };
+	}, 	
 };
 
 export default async function run(config?: CdsWebApi.Config, entity?: any) : Promise<any> {
@@ -49,7 +132,7 @@ export default async function run(config?: CdsWebApi.Config, entity?: any) : Pro
 	}
 
 	const api = new MetadataRepository(config);
-	const attributes = await api.retrieveAttributes(entity.MetadataId, [ 'AttributeType', 'AttributeTypeName', 'Description', 'DisplayName', 'IsCustomAttribute', 'IsPrimaryName', 'IsValidForCreate', 'LogicalName', 'SchemaName' ]);
+	const attributes = await api.retrieveAttributes(entity.MetadataId, []);
 	const returnFake: any = {
 		attributes,
 		generators: {},
@@ -79,13 +162,14 @@ export default async function run(config?: CdsWebApi.Config, entity?: any) : Pro
 	};
 
 	attributes.forEach(async a => {
-		switch (<number>(a.AttributeType)) {
-			case 1:
-				returnFake.generators[a.LogicalName] = await generators[(<number>a.AttributeType)](a, config);
+		switch (a.AttributeType) {
+			case "Customer":
+			case "Picklist":
+				returnFake.generators[a.LogicalName] = await generators[a.AttributeType](a, config);
 
 				break;
 			default:
-				returnFake.generators[a.LogicalName] = await generators[(<number>a.AttributeType)](a);
+				returnFake.generators[a.LogicalName] = await generators[a.AttributeType](a);
 
 				break;
 		}
--- src/repositories/DataApiRepository.ts
+++ src/repositories/DataApiRepository.ts
@@ -6,7 +6,67 @@ import ExtensionConfiguration from '../core/ExtensionConfiguration';
 import CdsRepository from '../api/CdsRepository';
 
 export default class DataApiRepository extends CdsRepository {
-    async getSampleCustomers(count: number = 25) : Promise<any> {
+    async getLookupValues(collection: string, count: number = 25): Promise<any[]> {
+        if (count < 1) { count = 1; }
+        if (count > 5000) { count = 5000; }
+
+        const entityData = await this.webapi.retrieveEntity(collection);        
+        const values = await this.webapi.retrieveMultipleRequest({ collection, top: count, select: [ entityData.PrimaryIdAttribute, entityData.PrimaryNameAttribute ] });
+
+        let returnArray = [];
+        
+        for (let i = 0; i < count; i++) { 
+            returnArray.push({ id: values[i][entityData.PrimaryIdAttribute], name: values[i][entityData.PrimaryNameAttribute], collection });
+        }
+
+        return returnArray;
+    }
+
+    async getPartyListMembers(count: number = 25): Promise<any[]> {
+        if (count < 1) { count = 1; }
+        if (count > 5000) { count = 5000; }
+
+        const accounts = await this.webapi.retrieveAllRequest({ collection: "accounts", select: [ "accountid", "name" ], top: count });
+        const contacts = await this.webapi.retrieveAllRequest({ collection: "contacts", select: [ "contactid", "name" ], top: count });
+        const users = await this.webapi.retrieveAllRequest({ collection: "systemusers", select: [ "systemuserid", "fullname" ], top: count });
+
+        let returnArray = [];
+
+        for (let i = 0; i < count; i++) {
+            const randomSeed = Math.round(Math.random() * 30);
+
+            if (randomSeed <= 10) {
+                const account = accounts[Math.round(Math.random() * accounts.length)];
+                returnArray.push({ id: account.accountid, entityType: "Account", name: account.name });
+            } else if (randomSeed <= 20) {
+                const contact = contacts[Math.round(Math.random() * contacts.length)];
+                returnArray.push({ id: contact.contactid, entityType: "Contact", name: contact.name });
+            } else {
+                const user = users[Math.round(Math.random() * users.length)];
+                returnArray.push({ id: user.systemuserid, entityType: "SystemUser", name: user.fullname });
+            }
+        }
+
+        return returnArray;
+    }
+
+    async getSystemUsers(count: number = 25): Promise<any[]> { 
+        if (count < 1) { count = 1; }
+        if (count > 5000) { count = 5000; }
+
+        const users = await this.webapi.retrieveAllRequest({ collection: "systemusers", select: [ "systemuserid", "fullname" ], top: count });
+
+        let returnArray = [];
+        
+        for (let i = 0; i < count; i++) {
+            const user = users[Math.round(Math.random() * users.length)];
+            returnArray.push({ id: user.systemuserid, collection: "systemuesrs", name: user.fullname });
+        }
+
+        return returnArray;
+    }
+
+    async getSampleCustomers(count: number = 25): Promise<any[]> {
         if (count < 1) { count = 1; }
         if (count > 5000) { count = 5000; }
         
